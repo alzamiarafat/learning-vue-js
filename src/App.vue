e8ef221107338701cbfb8a2f0a2ad66c9182c976
alzamiarafat <alzamiarafat00@gmail.com>
2021-08-28 21:02:55 +0600

    component and props

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,80 @@
 <template>
   <div>
-<!--    <p v-html="text"></p>-->
 <!--    <h3>Hello {{ name }}</h3>-->
-<!--    <p :id="textID">fdsihgf</p>-->
-<!--    <button :disabled="isDisabled">Apply</button>-->
-<!--    <p class="underLine">Under Line text</p>-->
-<!--    <p class="underLine" :class="status">gjsfh</p>-->
-<!--    <h3 :class="isPromoted && 'promoted'">dfghfh</h3>-->
-<!--    <h2 :class="isSoldout ? 'sold-out' : 'new'">stock</h2>-->
-<!--    <h2 :class="['promoted','new']">array</h2>-->
-<!--    <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">conditional array</h2>-->
-<!--    <h2 :class="{promoted: isPromoted,new:isSoldout,'sold_out':isSoldout}">conditional object</h2>-->
-<!--    <p :style="{color: highlightcolor,fontSize:headerSize+'px',padding: '15px'}">Inline style</p>-->
-<!--    <p :style="headerStyle">Style Object style</p>-->
-<!--    <h2 v-if="num === 0">The number is zero</h2>-->
-<!--    <h2 v-else>The number is not zero</h2>-->
+    <!--    <p :id="textID">fdsihgf</p>-->
+    <!--    <button :disabled="isDisabled">Apply</button>-->
+    <!--    <p class="underLine">Under Line text</p>-->
+    <!--    <p class="underLine" :class="status">gjsfh</p>-->
+    <!--    <h3 :class="isPromoted && 'promoted'">dfghfh</h3>-->
+    <!--    <h2 :class="isSoldout ? 'sold-out' : 'new'">stock</h2>-->
+    <!--    <h2 :class="['promoted','new']">array</h2>-->
+    <!--    <h2 :class="[isPromoted && 'promoted', isSoldout ? 'sold-out' : 'new']">conditional array</h2>-->
+    <!--    <h2 :class="{promoted: isPromoted,new:isSoldout,'sold_out':isSoldout}">conditional object</h2>-->
+    <!--    <p :style="{color: highlightcolor,fontSize:headerSize+'px',padding: '15px'}">Inline style</p>-->
+    <!--    <p :style="headerStyle">Style Object style</p>-->
+    <!--    <h2 v-if="num === 0">The number is zero</h2>-->
+    <!--    <h2 v-else>The number is not zero</h2>-->
 
 
-<!--    <h2 v-for="(name, index) in names" :key="name">{{ index+1}}=>{{ name }}</h2>
-    <h2 v-for="(name, index) in fullName" :key="name">{{ index+1}}.{{ name.firstName }} {{ name.lastName }}</h2>
-    <div v-for="(post, index) in posts" :key="post.post">
-      <h2>{{ index+1 }}.{{ post.post }}</h2>
-      <p v-for="comment in post.comments" :key="comment">{{ comment }}</p>
-    </div>
-    <h1>Conditional list view</h1>
-    <template v-for="name in names" :key="name">
-      <p v-if="name === 'Bruce' ">{{ name }}</p>
-    </template>-->
+    <!--    <p v-html="text"></p>-->
+    <!--    <h2 v-for="(name, index) in names" :key="name">{{ index+1}}=>{{ name }}</h2>
+        <h2 v-for="(name, index) in fullName" :key="name">{{ index+1}}.{{ name.firstName }} {{ name.lastName }}</h2>
+        <div v-for="(post, index) in posts" :key="post.post">
+          <h2>{{ index+1 }}.{{ post.post }}</h2>
+          <p v-for="comment in post.comments" :key="comment">{{ comment }}</p>
+        </div>
+        <h1>Conditional list view</h1>
+        <template v-for="name in names" :key="name">
+          <p v-if="name === 'Bruce' ">{{ name }}</p>
+        </template>-->
 
 
-<!--    <h2>Add Method {{ add(1,2,3) }}</h2>-->
 <!--    <h2>Multiply Method {{ multiply(20) }}</h2>-->
-<!--    <h2>Multiply Method {{ multiply(baseValue) }}</h2>-->
-<!--    <h1>{{ name }}</h1>-->
-<!--    <button v-on:click="name = 'superman'">Change</button>-->
-<!--    <h1> {{ counter }}</h1>-->
-<!--    <button @click="increment()">+</button>-->
-<!--    <button v-on:click="decrement()">-</button>-->
+    <!--    <h2>Multiply Method {{ multiply(baseValue) }}</h2>-->
+    <!--    <h1>{{ name }}</h1>-->
+    <!--    <button v-on:click="name = 'superman'">Change</button>-->
+    <!--    <h1> {{ counter }}</h1>-->
+    <!--    <button @click="increment()">+</button>-->
+    <!--    <button v-on:click="decrement()">-</button>-->
 
 
-<!--form handling-->
-<!--    <h2>{{ formValues }}</h2>-->
+    <!--form handling-->
+    <!--    <h2>{{ formValues }}</h2>-->
 
-<!--    <input type="number" v-model="formValues.text" placeholder="Click Here">-->
-<!--    <p>Input Here: {{ formValues.text+10 }}</p>-->
+    <!--    <input type="number" v-model="formValues.text" placeholder="Click Here">-->
+    <!--    <p>Input Here: {{ formValues.text+10 }}</p>-->
 
-<!--    <label>single selection</label>-->
-<!--    <select v-model="formValues.singleValue">-->
-<!--      <option value="red">red</option>-->
-<!--      <option value="blue">blue</option>-->
-<!--      <option value="green">green</option>-->
-<!--    </select>-->
-<!--    <h5>value {{ formValues.singleValue }}</h5>-->
+    <!--    <label>single selection</label>-->
+    <!--    <select v-model="formValues.singleValue">-->
+    <!--      <option value="red">red</option>-->
+    <!--      <option value="blue">blue</option>-->
+    <!--      <option value="green">green</option>-->
+    <!--    </select>-->
+    <!--    <h5>value {{ formValues.singleValue }}</h5>-->
 
 
-<!--    <label>single selection</label>-->
-<!--    <select v-model="formValues.multiValue" multiple>-->
-<!--      <option value="red">red</option>-->
-<!--      <option value="blue">blue</option>-->
-<!--      <option value="green">green</option>-->
-<!--    </select>-->
-<!--    <h5>value {{ formValues.multiValue }}</h5>-->
+    <!--    <label>single selection</label>-->
+    <!--    <select v-model="formValues.multiValue" multiple>-->
+    <!--      <option value="red">red</option>-->
+    <!--      <option value="blue">blue</option>-->
+    <!--      <option value="green">green</option>-->
+    <!--    </select>-->
+    <!--    <h5>value {{ formValues.multiValue }}</h5>-->
 
-<!--    <input type="checkbox" v-model="formValues.checked" id="work">-->
-<!--    <label for="work">work form home?</label><br>-->
+    <!--    <input type="checkbox" v-model="formValues.checked" id="work">-->
+    <!--    <label for="work">work form home?</label><br>-->
 
-<!--    <input type="checkbox" v-model="formValues.multipleCheck" value="html" id="html">-->
-<!--    <label for="html">HTML</label>-->
-<!--    <input type="checkbox" v-model="formValues.multipleCheck" value="css" id="css">-->
-<!--    <label for="css">CSS</label>-->
-<!--    <input type="checkbox" v-model="formValues.multipleCheck" value="jquery" id="jquery">-->
-<!--    <label for="jquery">JQUERY</label>-->
+    <!--    <input type="checkbox" v-model="formValues.multipleCheck" value="html" id="html">-->
+    <!--    <label for="html">HTML</label>-->
+    <!--    <input type="checkbox" v-model="formValues.multipleCheck" value="css" id="css">-->
+    <!--    <label for="css">CSS</label>-->
+    <!--    <input type="checkbox" v-model="formValues.multipleCheck" value="jquery" id="jquery">-->
+    <!--    <label for="jquery">JQUERY</label>-->
 
-<!--    <button @click="submit()">submit</button>-->
+    <!--    <button @click="submit()">submit</button>-->
 
-    <h2>Full Name: {{ firstName }} {{ lastName }}</h2>
-    <h3>Computed Full Name: {{ fullName }}</h3>
+    <Greet :firstName="firstName" :lastName="lastName" :fullName="fullName"/>
+
     <button @click="changeFullName('Hasib Arafat')">changeFullName</button>
 
     <P>Total: {{ items.reduce((total,crnt) => ( total += crnt.price ), 0) }}</P>
@@ -90,13 +89,16 @@
 
   </div>
 </template>
+<!--    <h2>Add Method {{ add(1,2,3) }}</h2>-->
 
 <script>
 
 /*import methods from "methods";*/
+import Greet from './components/Greet'
 
 export default {
   name: 'App',
+  components: {Greet},
   data() {
     return {
       firstName: "AL Zami",
---
+++ src/components/Greet.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Full Name: {{ firstName }} {{ lastName }}</h2>
+  <h3>Computed Full Name: {{ fullName }}</h3>
+  <h2>From Component</h2>
+</template>
+
+<script>
+export default {
+  name: "Greet",
+  props: ['firstName', 'lastName', 'fullName']
+}
+</script>
